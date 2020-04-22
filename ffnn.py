9ef009a21b362110d1d064af0575afb9760c6b52
Jaiganesh-MK <jaiganeshshs@gmail.com>
2020-04-22 23:23:04 +0530

    trained titanic dataset

--- ffnn.py
+++ ffnn.py
@@ -10,12 +10,17 @@ def two_layer_model(x,y,LEARNING_RATE,NUM_ITER):
 
     parameters = nn.initialize_parameters(size_input,size_hidden,size_output)
     costs = []
+
     for i in range(0,NUM_ITER):    
         ff = nn.feed_forward(parameters,x)
         a2 = ff["a2"]
         cos = nn.cost(a2,y)
         grads = nn.back_prop(ff,y,x)
         updated_parameters = nn.update_parameters(LEARNING_RATE,parameters,grads)
+        parameters["w1"] = updated_parameters["w1"]
+        parameters["w2"] = updated_parameters["w2"]
+        parameters["b1"] = updated_parameters["b1"]
+        parameters["b2"] = updated_parameters["b2"]
         if i % 100 == 0:
             print("Cost after iteration {}: {}".format(i, np.squeeze(cos)))
         if i % 100 == 0:
--- train.py
+++ train.py
@@ -6,10 +6,10 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("/home/mk/Desktop/feed_forward_neural_net/titanic.csv",header = None)
 y = df[0]
 x = df.drop([0],axis = 1)
-X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.2)
-X = X_train.to_numpy().T
-y = y_train.to_numpy().reshape(-1,1).T
-parameters = ffnn.two_layer_model(X,y,0.1,1000)
+X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state = 22)
+X = X_train.to_numpy()
+y = y_train.to_numpy().reshape(-1,1)
+parameters = ffnn.two_layer_model(X.T,y.T,0.0075,1000)
 
 
 
